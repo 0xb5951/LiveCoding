949a352e51a9a3d73e7ce414293597356c585bab
odrum428 <oxkeita@gmail.com>
2019-04-22 23:50:17 +0900

    全部の楽器を試そうとして死

---
+++ all_instrument.py
@@ -0,0 +1,71 @@
+# 使用できる楽器のリスト
+print(SynthDefs)
+
+lo >> play('x--o*=oo')
+
+sa >> sawbass()
+
+au >> audioin()
+
+no >> noise()
+
+da >> dab([0, 1, 5], dur=1/4)
+
+va >> varsaw()
+
+la >> lazer()
+
+gr >> growl()
+
+ba >> bass()
+
+di >> dirt()
+
+cr >> crunch()
+
+ra >> rave()
+
+
+sc >> scatter()
+
+ch >> charm()
+
+be >> bell()
+
+go >> gong()
+
+so >> soprano()
+
+du >> dub()
+
+vi >> viola()
+
+sc >> scratch()
+
+kl >> klank()
+
+fe >> feel()
+
+gl >> glass()
+
+so >> soft()
+
+qu >> quin()
+
+pu >> pluck()
+
+sp >> spark()
+
+bl >> blip()
+
+ri >> ripple()
+
+cr >> creep()
+
+o1 >> orient()
+
+za >> zap()
+
+ma >> marimba()
+
+fu >> fuzz()
