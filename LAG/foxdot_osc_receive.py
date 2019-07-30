9e55a8d817ce7a8a2fa033e35d78988d75c058b6
MizushimaKeita <mizushima.keita@lancers.co.jp>
2019-07-30 18:18:10 +0900

    LAGのリポジトリを作成して、foxdotのOSC受信部を作成

---
+++ LAG/foxdot_osc_receive.py
@@ -0,0 +1,10 @@
+from pythonosc import dispatcher
+from pythonosc import osc_server
+
+def processing(unused_addr, command):
+    exec(command)
+dispatcher = dispatcher.Dispatcher()
+dispatcher.map("/command", processing)
+
+server = osc_server.ThreadingOSCUDPServer(("127.0.0.1", 8000), dispatcher)
+server.serve_forever()
